7ee09153efba427db0c94069b99c2f4fc3164a24
Nickcau1203 <158166129+Nickcau1203@users.noreply.github.com>
2025-04-01 11:49:26 -0300

    faet: final do codigo

--- src/routes/personagemRoutes.js
+++ src/routes/personagemRoutes.js
@@ -1,22 +1,22 @@
 import express from "express";
 import PersonagemController from "../controllers/personagemController.js";
 
-const PersonagemRouter = express.Router();
+const personagensRouter = express.Router();
 
 // Rotas de Personagens
 // GET /personagens - Listar todos os Personagens
-personagensRouter.get("/", PersonagemController.getAllPersonagem);
+personagensRouter.get("/", PersonagemController.getAllPersonagens);
 
-// GET /api/Personagem/:id - Obter um Personagem pelo ID
+// GET /personagens/:id - Obter um Personagem pelo ID
 personagensRouter.get("/:id", PersonagemController.getPersonagemById);
 
-// POST /api/Personagem - Criar um novo Personagem
+// POST /personagens - Criar um novo Personagem
 personagensRouter.post("/", PersonagemController.createPersonagem);
 
-// PUT /api/Personagem/:id - Atualizar um Personagem
+// PUT /personagens/:id - Atualizar um Personagem
 personagensRouter.put("/:id", PersonagemController.updatePersonagem);
 
-// DELETE /api/Personagem/:id - Remover um Personagem
+// DELETE /personagens/:id - Remover um Personagem
 personagensRouter.delete("/:id", PersonagemController.deletePersonagem);
 
 export default personagensRouter;
